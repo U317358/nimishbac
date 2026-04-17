2b38813765c6342b2b0c3a0fcb7ad58e9cf08038
Utkarsha Shirsat <utk.shirsat@gmail.com>
2026-04-18 00:46:15 +0530

    update midlleware

--- backend/settings.py
+++ backend/settings.py
@@ -50,7 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
